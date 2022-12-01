ff4b0122440653ab5cfe6f82240683f4f654bf61
Zumag252 <artem2522462@mail.ru>
2022-12-01 18:08:01 +0900

    Произвел первый коммит

---
+++ src/Lessons11/lesson1/HomeLessons.java
@@ -0,0 +1,10 @@
+package Lessons11.lesson1;
+
+public class HomeLessons {
+    public static void main(String[] args) {
+        method();
+    }
+    public static void method() {
+        System.out.println("Изучение гита");
+    }
+}
